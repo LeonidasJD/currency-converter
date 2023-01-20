0e60d11ddc918e0600bb6b5df535d0cc59355b1e
Milan Djordjevic <milandjordjevic633@gmail.com>
2023-01-20 12:34:58 +0100

    adding new components

--- currency-converter/src/app/app.module.ts
+++ currency-converter/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { DescriptionComponent } from './description/description.component';
 import { ConverterComponent } from './converter/converter.component';
-import { ShowingCurrencyComponent } from './showing-currency/showing-currency.component';
+import { ShowingCurrencyComponent } from './converter/showing-currency/showing-currency.component';
+import { CurrencyListComponent } from './currency-list/currency-list.component';
+import { CurrencyItemComponent } from './currency-item/currency-item.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DescriptionComponent,
     ConverterComponent,
-    ShowingCurrencyComponent
+    ShowingCurrencyComponent,
+    CurrencyListComponent,
+    CurrencyItemComponent
   ],
   imports: [
     BrowserModule
---
+++ currency-converter/src/app/converter/currency-list/currency-item/currency-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-item',
+  templateUrl: './currency-item.component.html',
+  styleUrls: ['./currency-item.component.css']
+})
+export class CurrencyItemComponent {
+
+}
