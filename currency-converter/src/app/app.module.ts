6d2ade7a3fda66cb6b00ffc4763a5db136b295e5
Milan Djordjevic <milandjordjevic633@gmail.com>
2023-01-27 11:18:48 +0100

    added directive

--- currency-converter/src/app/app.module.ts
+++ currency-converter/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DescriptionComponent } from './description/description.component';
 import { MenuComponent } from './menu/menu/menu.component';
 import { GuardService } from './guard.service';
 import { FooterComponent } from './footer/footer.component';
+import { IncreaseHeightDirective } from './increase-height.directive';
 
 
 
@@ -43,7 +44,8 @@ const appRoutes: Routes = [
     CurrencyItemComponent,
     HomeComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    IncreaseHeightDirective
 
 
 
