22c78ccab19c698b8792989903a6e2bfec0cd0f4
Milan Djordjevic <milandjordjevic633@gmail.com>
2023-01-30 14:43:02 +0100

    added id for translate

--- currency-converter/src/app/currency-model/currency-model.ts
+++ currency-converter/src/app/currency-model/currency-model.ts
@@ -3,14 +3,16 @@ export class Currency {
 public name:string ;
 public flag: string;
 public shortName:string;
+public id:number;
 
 
 
-constructor(name:string, flag:string,shortName:string) {
+constructor(name:string, flag:string,shortName:string,id:number) {
 
 this.name = name;
 this.flag = flag;
 this.shortName = shortName;
+this.id = id;
 
 
 
