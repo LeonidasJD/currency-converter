5761d7e1620c559e6311775ad8be638a6dcc17ca
Milan Djordjevic <milandjordjevic633@gmail.com>
2023-01-31 14:50:05 +0100

    added rate in model

--- currency-converter/src/app/converter/convert.service.ts
+++ currency-converter/src/app/converter/convert.service.ts
@@ -31,37 +31,43 @@ export class ConvertService {
       'Euro',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Flag_of_Europe.svg/231px-Flag_of_Europe.svg.png',
       'EUR',
-      0
+      0,
+      '117,37'
     ),
     new Currency(
       'Franc',
       'https://www.svajcarska.com/wp-content/uploads/2018/04/Flag_of_Switzerland-300x300.png',
       'CHF',
-      1
+      1,
+      '117.23'
     ),
     new Currency(
       'Dollar',
       'https://cdn.britannica.com/33/4833-004-828A9A84/Flag-United-States-of-America.jpg',
       'USD',
-      2
+      2,
+      '108.01'
     ),
     new Currency(
       'Dollar',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_Canada.svg/1920px-Flag_of_Canada.svg.png',
       'CAD',
-      3
+      3,
+      '81.01'
     ),
     new Currency(
       ' Yen',
       "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_the_People%27s_Republic_of_China.svg/1920px-Flag_of_the_People%27s_Republic_of_China.svg.png",
       'CNY',
-      4
+      4,
+      '15.99'
     ),
     new Currency(
       'Ruble',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Russia.svg/255px-Flag_of_Russia.svg.png',
       'RUB',
-      5
+      5,
+      '1.53'
     ),
 
   ];
